c7faa8faa4e2f071f27a2b55dbea5cd1475b030c
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2026-01-21 21:50:53 -0500

    added boolean operators into operatortype

--- RedstoneScript/Models/OperatorType.cs
+++ RedstoneScript/Models/OperatorType.cs
@@ -14,4 +14,22 @@ public static class OperatorType
     public const string DIVISION = "/";
 
     public const string MODULUS = "%";
+
+    public const string EQUALS = "==";
+    
+    public const string NOTEQUAL = "!=";
+
+    public const string LESSTHAN = "<";
+
+    public const string LESSTHANEQUALTO = "<=";
+
+    public const string GREATERTHAN = ">";
+
+    public const string GREATERTHANEQUALTO = ">=";
+
+    public const string AND = "&&";
+
+    public const string OR = "||";
+
+    public const string UNARYNOT = "!";
 }
